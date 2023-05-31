e2a47cf91f1d562ec83ef9aee01dd6b0a1edd4a6
johnathanbryce <johnathanbryce@rompathlete.com>
2023-05-31 15:43:28 -0700

    created slideshow hero component

--- src/components/SlideshowAuto/SlideShowAuto.tsx
+++ src/components/SlideshowAuto/SlideShowAuto.tsx
@@ -8,31 +8,30 @@ import slide2 from '../../../public/images/slide2.jpg'
 import slide3 from '../../../public/images/slide3.jpg'
 import slide4 from '../../../public/images/slide4.jpg'
 
-// slides could be exported to a list under a "lists" folder 
-  const slides = [
-    {
-      image: slide1,
-      header: 'Slide 1',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum semper magna, nec ullamcorper velit viverra nec.'
-    },
-    {
-      image: slide2,
-      header: 'Slide 2',
-      text: 'Quisque luctus diam nec finibus eleifend. Morbi vel purus in est pulvinar efficitur.'
-    },
-    {
-      image: slide3,
-      header: 'Slide 3',
-      text: 'Praesent consequat libero in mi facilisis, sed bibendum enim dictum. Maecenas maximus vestibulum dignissim..'
-    },
-    {
-      image: slide4,
-      header: 'Slide 4',
-      text: 'Nunc aliquet leo et mi hendrerit, id efficitur purus congue. Nulla vel velit ut lectus congue vehicula.'
-    },
-    // Add more slides as needed
-  ];
-
+// slides could be exported to a slidesList.js file in a "lists" folder 
+const slides = [
+  {
+    image: slide1,
+    header: 'Slide 1',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum semper magna, nec ullamcorper velit viverra nec.'
+  },
+  {
+    image: slide2,
+    header: 'Slide 2',
+    text: 'Quisque luctus diam nec finibus eleifend. Morbi vel purus in est pulvinar efficitur.'
+  },
+  {
+    image: slide3,
+    header: 'Slide 3',
+    text: 'Praesent consequat libero in mi facilisis, sed bibendum enim dictum. Maecenas maximus vestibulum dignissim..'
+  },
+  {
+    image: slide4,
+    header: 'Slide 4',
+    text: 'Nunc aliquet leo et mi hendrerit, id efficitur purus congue. Nulla vel velit ut lectus congue vehicula.'
+  },
+  // Add more slides as needed
+];
 
 const SlideShowAuto = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
